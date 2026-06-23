bc38857de17684ae716f8afda159cb241ede8ed6
fourthstanza <kitkatmatthew@hotmail.com>
2026-06-22 20:46:34 -0400

    added voltage measuring

--- main/main.c
+++ main/main.c
@@ -1,22 +1,23 @@
-
 #include <stdio.h>
 #include "main.h"
 #include "board.h"
 
 static const char *TAG = "main";
 
-void commWithSensor(void){
+//void commWithSensor(void){
 
-}
+//}
 
 void sensorPollTask(void *pvParameters)
 {
     i2c_master_dev_handle_t bno_handle = (i2c_master_dev_handle_t)pvParameters;
+    adc_oneshot_unit_handle_t adc_handle = (adc_oneshot_unit_handle_t)pvParameters;
     
     while (1) {
-        int16_t ax, ay, az;
-        int16_t gx, gy, gz;
-        int16_t mx, my, mz;
+        static int16_t ax, ay, az;
+        static int16_t gx, gy, gz;
+        static int16_t mx, my, mz;
+        static int voltage[1];
 
         esp_err_t accel_err;
         esp_err_t gyro_err;
@@ -26,22 +27,20 @@ void sensorPollTask(void *pvParameters)
         gyro_err = bno055_get_gyro(bno_handle, &gx, &gy, &gz);
         mag_err = bno055_get_mag(bno_handle, &mx, &my, &mz);
 
-        
+        lipo_get_voltage(adc_handle, voltage);
 
-        ESP_LOGI(TAG, "Accel: ax=%d, ay=%d, az=%d\nGyro: gx=%d, gy=%d, gz=%d\nMag: mx=%d, my=%d, mz=%d", ax, ay, az, gx, gy, gz, mx, my, mz);
+
+        ESP_LOGI(TAG, "Accel: ax=%d, ay=%d, az=%d\nGyro: gx=%d, gy=%d, gz=%d\nMag: mx=%d, my=%d, mz=%d, voltage=%d", ax, ay, az, gx, gy, gz, mx, my, mz, voltage[0]);
         vTaskDelay(pdMS_TO_TICKS(20));
     }
 }
 
-void logTask(void *pvParameters)
-{
+//void logTask(void *pvParameters)
+//{
     
-}
+//}
 
-void app_main(void)
-{
-    beeper_init(BEEPER_IO);
-    i2c_master_bus_handle_t bus_handle;
+esp_err_t i2c_bus_init(i2c_master_bus_handle_t *handle) {
     i2c_master_bus_config_t bus_config = {
         .i2c_port = I2C_MASTER_NUM,
         .sda_io_num = I2C_MASTER_SDA_IO,
@@ -50,18 +49,27 @@ void app_main(void)
         .glitch_ignore_cnt = 7,
         .flags.enable_internal_pullup = false,
     };
-    ESP_ERROR_CHECK(i2c_new_master_bus(&bus_config, &bus_handle));
+    return(i2c_new_master_bus(&bus_config, handle));
+}
+
+void app_main(void)
+{
+    beeper_init(BEEPER_IO);
+
+    adc_oneshot_unit_handle_t adc_handle;
+    lipo_meas_init(LIPO_IO, &adc_handle);
+    i2c_master_bus_handle_t bus_handle;
+    esp_err_t i2c_bus_err = i2c_bus_init(&bus_handle);
     i2c_master_dev_handle_t bno_handle;
     bno055_i2c_init(&bus_handle, &bno_handle);
-    ESP_ERROR_CHECK(bno055_who_am_i(bno_handle));
-
+    esp_err_t bno_err = bno055_who_am_i(bno_handle);
     ESP_LOGI(TAG, "I2C initialized successfully");
 
     vTaskDelay(pdMS_TO_TICKS(700)); // bno055Power-Off Reset time is 650ms
 
     ESP_ERROR_CHECK(bno055_init(bno_handle, ORIENT_XYZ));
 
-    mg90s_init(27, 1);
+    //mg90s_init(27, 1);
 
     xTaskCreate(sensorPollTask,"sensor_read_task", 4096, bno_handle, tskIDLE_PRIORITY + 2, NULL);
     //xTaskCreate(logTask,"log_task", 4096, NULL, tskIDLE_PRIORITY + 1, NULL);
--- main/main.h
+++ main/main.h
@@ -6,6 +6,10 @@
 #include "bno055.h"
 #include "beeper.h"
 #include "mg90s.h"
+#include "led_contr.h"
+#include "soc/adc_channel.h"
+#include "lipo_meas.h"
+#include "esp_adc/adc_oneshot.h"
 
 #define SensorPollPriority          tskIDLE_PRIORITY + 2
 #define LogPriority                 tskIDLE_PRIORITY + 1
