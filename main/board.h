10d74558e7817f6cbcc991767dfc8b70230124d9
fourthstanza <kitkatmatthew@hotmail.com>
2026-06-26 15:37:58 -0400

    moved battery voltage references to component

--- main/board.h
+++ main/board.h
@@ -9,6 +9,3 @@
 #define I2C_MASTER_FREQ_HZ          CONFIG_I2C_MASTER_FREQUENCY /* I2C master clock frequency */
 #define I2C_MASTER_TX_BUF_DISABLE   0                           /* I2C master doesn't need buffer */
 #define I2C_MASTER_RX_BUF_DISABLE   0                           /* I2C master doesn't need buffer */
-
-#define V_BATT_MAX                  1.707                       /* Full battery reference voltage for ADC */
-#define V_BATT_MIN                  1.300                       /* Empty battery reference voltage for ADC */
