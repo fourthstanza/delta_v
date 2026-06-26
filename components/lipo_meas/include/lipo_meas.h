3cab75aa8e78db3b8d8f9b8253b43bfb83c05804
fourthstanza <kitkatmatthew@hotmail.com>
2026-06-26 15:37:32 -0400

    refactored voltage measuring component

--- components/lipo_meas/include/lipo_meas.h
+++ components/lipo_meas/include/lipo_meas.h
@@ -2,18 +2,15 @@
 #include "esp_log.h"
 #include "esp_adc/adc_oneshot.h"
 
-#define ADC_BUF_SIZE                1024
-#define ADC_FRAME_SIZE              256
 #define ADC_CHANNEL                 ADC1_GPIO36_CHANNEL
-#define ADC_ATTEN                   3                           /* 150 to 2450 mV*/
-#define ADC_UNIT                    0
-#define ADC_BITWIDTH                10
+#define ADC_ATTEN                   ADC_ATTEN_DB_12             /* 150 to 2450 mV*/
+#define ADC_BITWIDTH                ADC_BITWIDTH_10
 #define ADC_CLK_SRC                 0
-#define ADC_NO_ULP                  0
+#define ADC_NO_ULP                  ADC_ULP_MODE_DISABLE
 #define V_REF                       1100
 
-#define V_BATT_MAX                  1.707                       /* Full battery reference voltage for ADC */
-#define V_BATT_MIN                  1.300                       /* Empty battery reference voltage for ADC */
+#define V_BATT_MAX                  1680                       /* Full battery reference voltage for ADC */
+#define V_BATT_MIN                  1280                       /* Empty battery reference voltage for ADC */
 
-void lipo_meas_init(uint8_t gpio, adc_oneshot_unit_handle_t *adc_handle);
-void lipo_get_voltage(adc_oneshot_unit_handle_t adc_handle, int *data);
+void lipo_meas_init(adc_oneshot_unit_handle_t *adc_handle);
+void lipo_get_voltage(adc_oneshot_unit_handle_t adc_handle, int *voltage);
