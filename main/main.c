ab6d243271c6368a7b5a0bbf8b1bf29c70452a96
fourthstanza <kitkatmatthew@hotmail.com>
2026-09-21 20:13:49 -0400

    renamed master bus handle

--- main/main.c
+++ main/main.c
@@ -38,7 +38,7 @@ void sensorPollTask(void *pvParameters)
         vTaskDelay(pdMS_TO_TICKS(20));
     }
 }
- 
+
 //void logTask(void *pvParameters)
 //{
     
@@ -97,16 +97,15 @@ void ledTask(void *pvParameters)
     }
 }
 
-esp_err_t i2c_bus_init(i2c_master_bus_handle_t *handle) {
+esp_err_t i2c_bus_init(i2c_master_bus_handle_t *bus_handle) {
     i2c_master_bus_config_t bus_config = {
         .i2c_port = I2C_MASTER_NUM,
         .sda_io_num = I2C_MASTER_SDA_IO,
         .scl_io_num = I2C_MASTER_SCL_IO,
         .clk_source = I2C_CLK_SRC_DEFAULT,
-        .glitch_ignore_cnt = 7,
         .flags.enable_internal_pullup = false,
     };
-    return(i2c_new_master_bus(&bus_config, handle));
+    return(i2c_new_master_bus(&bus_config, bus_handle));
 }
 
 void app_main(void)
